4b9969bdec31280bc2284f4fd2d9c9f73e0ef3fa
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-15 23:49:08 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,11 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.create
+    @student.first_name = params[:student][:first_name]
+    @student.last_name = params[:student][:last_name]
+    @student.save
+    redirect_to student_path(@student)
   end
 
 end
